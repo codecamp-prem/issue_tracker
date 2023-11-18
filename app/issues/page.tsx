73b99583aa3f9e56c7dbcf771fcb50c7759d732f
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-11-18 14:43:58 +0545

    build a new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPages = () => {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
