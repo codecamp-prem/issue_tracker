c210e7809be2bd41eb66423050f0bd521d2fe672
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-11-19 16:14:30 +0545

    handling errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,15 +3,15 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Issue Title is required.").max(255),
+  description: z.string().trim().min(1, "Issue detail Description is required"),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIsssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
