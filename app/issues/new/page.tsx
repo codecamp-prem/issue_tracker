f2b98f63bdf39de19a8cfced602c7a4fa7b7b80b
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-11-21 18:06:25 +0545

    extracting the errorMsg in seprate component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
@@ -9,6 +9,7 @@ import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 // interface IssueForm {
 //   title: string;
 //   description: string;
@@ -47,11 +48,7 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Issue Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -59,11 +56,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="type your issue here" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
