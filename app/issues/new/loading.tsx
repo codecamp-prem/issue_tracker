47c6c0cdefff9eec9c070e0dacdf3951ac445803
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-11-23 17:09:47 +0545

    add loading skeleton and test with delay in pages

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const LoadingCreateNewIssuePage = () => {
-  return <div>loading...</div>;
+const LoadingCreateNewIssuePage = async () => {
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
+      <Skeleton width="5rem" />
+    </Box>
+  );
 };
 
 export default LoadingCreateNewIssuePage;
