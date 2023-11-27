bda80444ac5502d77024cc8d4d311b79f76a0680
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-11-27 16:23:08 +0545

    improve the loading experience

---
+++ app/issues/_components/IssueFormSkeleton.tsx
@@ -0,0 +1,14 @@
+import { Skeleton } from "@/app/components/";
+import { Box } from "@radix-ui/themes";
+
+const IssueFormSkeleton = () => {
+  return (
+    <Box className="max-w-xl">
+      <Skeleton height="2rem" />
+      <Skeleton height="20rem" />
+      <Skeleton height="2rem" width="5rem" />
+    </Box>
+  );
+};
+
+export default IssueFormSkeleton;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,14 +1,3 @@
-import { Skeleton } from "@/app/components";
-import { Box } from "@radix-ui/themes";
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
 
-const LoadingCreateNewIssuePage = async () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton />
-      <Skeleton height="20rem" />
-      <Skeleton width="5rem" />
-    </Box>
-  );
-};
-
-export default LoadingCreateNewIssuePage;
+export default IssueFormSkeleton;
