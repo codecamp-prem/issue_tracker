3fffa4a34eaa96160616450cb0fadf9f2cecca83
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-12-08 13:37:44 +0545

    setup next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>Hello World!</div>;
+  const [bill, setBill] = useState(50);
+  const [tipPercentage, setTipPercentage] = useState(18);
+  const [people, setPeople] = useState(1);
+
+  const totalTip = (bill * tipPercentage) / 100;
+  const perPersonTip = totalTip / people;
+  return (
+    <>
+      <label htmlFor="bill">Bill : </label>
+      <input
+        id="bill"
+        type="number"
+        min="0"
+        value={bill}
+        onChange={(event) => {
+          setBill(parseInt(event.target.value));
+        }}
+      />
+      <label htmlFor="tipPercentage">Tip Percentage : </label>
+      <input
+        id="tipPercentage"
+        type="number"
+        min="0"
+        value={tipPercentage}
+        onChange={(event) => {
+          setTipPercentage(parseInt(event.target.value));
+        }}
+      />
+      <label htmlFor="people">People : </label>
+      <input
+        id="people"
+        type="number"
+        min="0"
+        value={people}
+        onChange={(event) => {
+          setPeople(parseInt(event.target.value));
+        }}
+      />
+      <p>Total tip: {isNaN(totalTip) ? "-" : `$${totalTip.toFixed(2)}`}</p>
+      <p>
+        Tip Per Person:{" "}
+        {isNaN(perPersonTip) ? "-" : `$${perPersonTip.toFixed(2)}`}
+      </p>
+    </>
+  );
 }
