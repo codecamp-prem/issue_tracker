6bc0d9e431436828c492cf4b4638bf6964335f72
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-12-09 14:02:07 +0545

    modify api to accept assign issue to user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import AuthOptions from "@/app/auth/AuthOptions";
-import { issueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -9,14 +9,21 @@ export async function PATCH(
   { params }: { params: { id: string } }
 ) {
   const session = await getServerSession(AuthOptions);
-
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -30,6 +37,7 @@ export async function PATCH(
     data: {
       title: body.title,
       description: body.description,
+      assignedToUserId: body.assignedToUserId,
     },
   });
 
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,5 +2,25 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
   title: z.string().min(1, "Issue Title is required.").max(255),
-  description: z.string().trim().min(1, "Issue detail Description is required"),
+  description: z
+    .string()
+    .trim()
+    .min(1, "Issue detail Description is required")
+    .max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Issue Title is required.").max(255).optional(),
+  description: z
+    .string()
+    .trim()
+    .min(1, "Issue detail Description is required")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "Assigned to UserId is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
