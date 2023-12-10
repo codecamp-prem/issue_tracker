d6e907cbcfe5ab99f90d31d729830677f246cb65
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-12-10 12:27:55 +0545

    make issue columns sortable asc

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 // See schema.prisma Issue model , since sqlite doesn't support enum
 const all_status: { label: string; value: 1 | 2 | 3 | 0 }[] = [
@@ -11,12 +11,21 @@ const all_status: { label: string; value: 1 | 2 | 3 | 0 }[] = [
 ];
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams(); // hook to get current query parameters
+
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        let queryParam = "";
+        const params = new URLSearchParams();
+
         if (status == "1" || status == "2" || status == "3")
-          queryParam = `?status=${status}`;
+          params.append("status", status);
+
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        const queryParam = params.size ? "?" + params.toString() : "";
+
         router.push("/issues/list" + queryParam);
       }}
     >
