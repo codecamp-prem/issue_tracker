7ebae390825e1722b9b9ec877003f29b6bd141f7
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-12-10 20:06:10 +0545

    refactor: extract issue Table

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,22 +1,13 @@
-import { IssueStatusBadge, Link } from "@/app/components";
 import Pagination from "@/app/components/Pagination";
 import prisma from "@/prisma/client";
-import { Issue } from "@prisma/client";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
-import { Table } from "@radix-ui/themes";
-import NextLink from "next/link";
+import { Flex } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
+import IssueTable, { IssueQuery, columnNames } from "./IssueTable";
 
 type Props = {
-  searchParams: { status: string; orderBy: keyof Issue; page: string };
+  searchParams: IssueQuery;
 };
 const IssuesPages = async ({ searchParams }: Props) => {
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: "Issue", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ];
-
   let status = undefined; // undefined: prisma will not pass it as a part of filtering
   if (searchParams.hasOwnProperty("status")) {
     // sqlite doesn't support enum, See schema.prisma Issue model for status column
@@ -25,16 +16,14 @@ const IssuesPages = async ({ searchParams }: Props) => {
     if (searchParams.status == "3") status = 3;
   }
 
-  const orderBy = columns
-    .map((column) => column.value)
-    .includes(searchParams.orderBy)
+  const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: "asc" }
     : undefined;
 
   const page = parseInt(searchParams.page) || 1;
   const pageSize = 10;
 
-  const isssues = await prisma.issue.findMany({
+  const issuesFromDb = await prisma.issue.findMany({
     where: {
       status,
     },
@@ -46,56 +35,15 @@ const IssuesPages = async ({ searchParams }: Props) => {
   const totalIssueCount = await prisma.issue.count({ where: { status } });
 
   return (
-    <div>
+    <Flex direction="column" gap="3">
       <IssueActions />
-
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell
-                key={column.value}
-                className={column.className}
-              >
-                <NextLink
-                  href={{
-                    query: { ...searchParams, orderBy: column.value },
-                  }}
-                >
-                  {column.label}
-                </NextLink>
-                {column.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {isssues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueTable searchParams={searchParams} issues={issuesFromDb} />
       <Pagination
         pageSize={pageSize}
         currentPage={page}
         itemCount={totalIssueCount}
       />
-    </div>
+    </Flex>
   );
 };
 
